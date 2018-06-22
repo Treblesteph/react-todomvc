11addc1177f94bbf793080c4a75caa5baa69c31c
Stephanie Unna <stephanie.u@mmtdigital.co.uk>
2018-06-22 12:17:42 +0100

    Setup redux state management

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
 import MyComponent from './components/MyComponent'
 import './styles/styles.scss'
+import reducers from './reducers';
 
-ReactDOM.render(<MyComponent />, document.getElementById('root'))
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <MyComponent />
+  </Provider>
+  , document.getElementById('root'))
