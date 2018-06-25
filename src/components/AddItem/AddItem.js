6b197216490fe598fa6d39627cef3c17cd1481ab
Stephanie Unna <steph.unna@gmail.com>
2018-06-25 10:24:03 +0100

    Adding event handler for add item enter key press

--- src/components/AddItem/AddItem.js
+++ src/components/AddItem/AddItem.js
@@ -3,15 +3,11 @@ import React, { Component } from 'react'
 import './AddItem.scss'
 
 class AddItem extends Component {
-  handleKeyPress = (e) => {
-    if (e.key === 'Enter') {
-      console.log('added!')
-    }
-  }
+  handleKeyPress = (e) => {(e.key === 'Enter' ? alert(e.target.value) : null)};
   render () {
     return (
       <React.Fragment>
-        <form onSubmit={console.log("new todo")}>
+        <form>
           <input type="text" onKeyPress={this.handleKeyPress} name="additem" placeholder="What needs to be done?"></input>
         </form>
       </React.Fragment>
