f677c4b29b550aeb28cbaa8cbf14b9935963c195
Stephanie Unna <steph.unna@gmail.com>
2018-06-25 09:02:25 +0100

    Organising component folder structure

--- src/components/AddItem/AddItem.js
+++ src/components/AddItem/AddItem.js
@@ -4,7 +4,7 @@ class AddItem extends Component {
   render () {
     return (
       <React.Fragment>
-        <form>
+        <form onSubmit={console.log("new todo")}>
           <input type="text" name="additem" placeholder="What needs to be done?"></input>
         </form>
       </React.Fragment>
