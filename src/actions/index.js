56755df4934ee310abf5514804b495bf2f5f9e73
Stephanie Unna <steph.unna@gmail.com>
2018-06-26 11:23:23 +0100

    Linter stuff, imports and exports

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,24 +9,19 @@ const addNewTodo = content => ({
 });
 
 const setVisibilityFilter = filter => ({
-  type: 'SET_VISIBILITY_FILTER',
+  type: "SET_VISIBILITY_FILTER",
   filter
 })
 
 const toggleTodo = id => ({
   type: 'TOGGLE_TODO',
   id
-})
+});
 
 const VisibilityFilters = {
-  SHOW_ALL: 'SHOW_ALL',
-  SHOW_COMPLETED: 'SHOW_COMPLETED',
-  SHOW_ACTIVE: 'SHOW_ACTIVE'
-}
+  SHOW_ALL: "SHOW_ALL",
+  SHOW_COMPLETED: "SHOW_COMPLETED",
+  SHOW_ACTIVE: "SHOW_ACTIVE"
+};
 
-export {
-  addTodo,
-  setVisibilityFilter,
-  toggleTodo,
-  VisibilityFilters
-}
+export { addNewTodo, toggleTodo, setVisibilityFilter, updateTodo, VisibilityFilters };
