924025fb0c89c15596f6882b8c51c65a0b87bde6
Stephanie Unna <steph.unna@gmail.com>
2018-06-26 11:30:36 +0100

    Make todo item editable on double click

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -4,9 +4,22 @@ const Todo = ({ handleClick, handleKeyPress, todo }) => (
   <li>
     <input onClick={handleClick} type="checkbox" id={todo.id} name="todoitem" value="item1" />
     <label
-      for={key}
-      style={{textDecoration: completed ? 'line-through' : 'none'}}
-    >{content}</label>
+      htmlFor={todo.id}
+      style={{textDecoration: todo.completed ? 'line-through' : 'none'}}
+    >
+      <p
+        onDoubleClick={(e) => (
+          e.target.contentEditable=true,
+          e.target.className='inEdit',
+          e.target.onKeyPress={handleKeyPress}
+        )}
+        onBlur={(e) => (e.target.contentEditable=false, e.target.className='')}
+        contentEditable="false"
+        className=""
+      >
+        {todo.content}
+      </p>
+    </label>
   </li>
 )
 
