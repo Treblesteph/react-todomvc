70aa258eb269bff6348b149ed43ba0f4cf3b7043
Stephanie Unna <steph.unna@gmail.com>
2018-06-26 08:48:00 +0100

    Putting in visibleItems to list, commenting out buttons for now

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,29 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import './ItemList.scss'
+import "./ItemList.scss";
+
+// const mapStateToProps = state => ({
+//   visibleTodos: getVisibleTodos(state.todos, state.visibilityFilter)
+// })
+
+const Items = visibleTodos =>
+  <ul>
+    {visibleTodos.map(todo =>
+      <TodoItem key={todo.id} {...todo} onClick={() => toggleTodo(todo.id)} />
+    )}
+  </ul>;
 
 class ItemList extends Component {
-  render () {
+  render() {
+    const { visibleTodos } = this.props;
     return (
       <form>
-        <div>
-          <input type="checkbox" id="item1" key="item1" name="todoitem"
-                 value="item1" checked />
-          <label for="item1">Item 1</label>
-        </div>
-        <div>
-            <input type="checkbox" id="item2" key="item2" name="todoitem"
-                  value="item2" />
-            <label for="item2">Item 2</label>
-        </div>
-        <div>
-            <input type="checkbox" id="item3" key="item3" name="todoitem"
-                  value="item3" />
-            <label for="item3">Item 3</label>
-        </div>
+        {Items(visibleTodos)}
       </form>
-    )
+    );
   }
 }
 
-export default ItemList
+export default ItemList;
