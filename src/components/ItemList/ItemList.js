f41136c51209fdfe1d68312bc9f4722b5e0468da
Stephanie Unna <steph.unna@gmail.com>
2018-06-26 11:28:47 +0100

    Starting on handle keypress for editTodo

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -4,6 +4,20 @@ import "./ItemList.scss";
 
 
 class ItemList extends Component {
+  handleKeyPress = (e) => {
+    e.key !== 'Enter' ? (
+      console.log('different key pressed'),
+      null) : (
+        !e.target.value.trim() ? (
+          console.log(e),
+          console.log('this should delete the item'), 
+        null) : (
+          console.log('editing item'),
+          e.preventDefault(),
+          this.props.editTodo(e.target.value)
+        )
+    )
+  }
   render() {
     const { visibleTodos } = this.props;
     return (
